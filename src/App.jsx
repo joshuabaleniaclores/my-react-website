b581ae9c1d67e8453416aee2bdbc2bc57e2c89e8
Joshua Clores <jclores@payremit.com>
2025-02-17 15:00:01 +0800

    [IN PROGRESS] - About

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./index.css";
 
 import NavigationBar from "./components/navigation-bar";
 import Home from "./components/home";
+import About from "./components/about";
 
 function App() {
   return (
     <>
       <NavigationBar />
       <Home />
+      <About />
     </>
   );
 }
---
+++ src/components/about.jsx
@@ -0,0 +1,20 @@
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="flex justify-center items-center w-full h-screen"
+    >
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold">About Me</h1>
+        <article className="text-justify text-xl">
+          Hi! I&apos;m Joshua Clores, a software engineer who specializes in
+          creating websites and applications. I work as a Junior Software
+          Enginner at Ventaja International Corporation. I like keeping up with
+          the latest technology and design trends.
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default About;
